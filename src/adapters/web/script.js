a02c272932845220243bcea7e607368e99591dbc
dek <derek.svart@gmail.com>
2024-09-13 17:16:45 +0200

    Add GET end-point.

--- src/adapters/web/script.js
+++ src/adapters/web/script.js
@@ -1,9 +1,22 @@
 import axios from 'https://cdn.jsdelivr.net/npm/axios@1.3.5/+esm';
 
-document.getElementById('encryptForm').addEventListener('submit', async (e) => {
+document.getElementById('encryptFormGet').addEventListener('submit', async (e) => {
     e.preventDefault();
-    const text = document.getElementById('text').value;
-    const shift = parseInt(document.getElementById('shift').value);
+    const text = document.getElementById('textGet').value;
+    const shift = document.getElementById('shiftGet').value;
+
+    try {
+        const response = await axios.get('/encrypt', { params: { text, shift } });
+        document.getElementById('result').textContent = `Encrypted text: ${response.data.encryptedText}`;
+    } catch (error) {
+        document.getElementById('result').textContent = `Error: ${error.response.data.error}`;
+    }
+});
+
+document.getElementById('encryptFormPost').addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const text = document.getElementById('textPost').value;
+    const shift = parseInt(document.getElementById('shiftPost').value);
 
     try {
         const response = await axios.post('/encrypt', { text, shift });
